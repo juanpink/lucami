2201d651917a3a0b74da423558062cb455531069
juanpink <jmiopin6@gmail.com>
2025-01-24 18:13:34 +0100

    Add files via upload

--- amigurumi.js
+++ amigurumi.js
@@ -36,9 +36,47 @@ function generaDetalle(amigurumi) {
     nuevaClase.textContent = "Clase : " + amigurumi.clase;
     nuevoSectionTexto.appendChild(nuevaClase);
 
+    //Aquí se crea el nuevo div con las miniaturas de las imágenes si las hay, de momento siempre hay
+
+    //const imagenes = [];
+    //const imagenes = ["./img/gato1.jpg", "./img/gato2.jpg", "./img/gato3.jpg"];
+    //console.log("IMAGENES --> ", amigurumi.imagenes)
+    const imagenes = amigurumi.imagenes;
+
+    if (imagenes.length > 0) {
+        const img1 = imagenes[0];
+        const img2 = imagenes[1];
+        const img3 = imagenes[2];
+
+        const alt1 = img1.replace('./img/', '').replace('.jpg', '');
+        const alt2 = img2.replace('./img/', '').replace('.jpg', '');
+        const alt3 = img3.replace('./img/', '').replace('.jpg', '');
+
+        //4. Crear el div con las miniaturas de las imágenes
+        const nuevasMiniaturas = document.createElement("div");
+        nuevasMiniaturas.classList.add('row');
+        nuevasMiniaturas.innerHTML = `<div class="column"><img src=${img1} width="150px" onclick="openModal();currentSlide(1)" class="hover-shadow" id="img1"><div>`;
+        nuevasMiniaturas.innerHTML += `<div class="column"><img src=${img2} width="150px" onclick="openModal();currentSlide(1)" class="hover-shadow" id="img1"><div>`;
+        nuevasMiniaturas.innerHTML += `<div class="column"><img src=${img3} width="150px" onclick="openModal();currentSlide(1)" class="hover-shadow" id="img1"><div>`;
+        nuevoSectionTexto.appendChild(nuevasMiniaturas);
+
+        //Modifica los datos del modal
+        document.querySelector("#myslide1").src=img1;
+        document.querySelector("#myslide2").src=img2;
+        document.querySelector("#myslide3").src=img3;
+
+        document.querySelector("#thumbail1").src=img1;
+        document.querySelector("#thumbail2").src=img2;
+        document.querySelector("#thumbail3").src=img3;
+
+        document.querySelector("#thumbail1").alt=alt1;
+        document.querySelector("#thumbail2").alt=alt2;
+        document.querySelector("#thumbail3").alt=alt3;
+    }
 
     //Añade la section a la derecha
     nuevoArticulo.appendChild(nuevoSectionTexto);
+
 }
 
 document.querySelector("#volver").addEventListener("click", () => {
